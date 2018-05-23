277092857fc59794be3c9359b867e4b0d924e450
Mit <mit_rupang@yahoo.ca>
2018-05-23 09:56:24 -0400

    Fixed footer and header styling

--- src/App.js
+++ src/App.js
@@ -16,46 +16,48 @@ import Conhall from './Images/conhall.svg';
 
 class App extends Component {
   render() {
-    return <Grid fluid>
-      <Row>
-        <Header/>
-      </Row>
-      <Row style={{minHeight: '100vh'}}>
-        <Col xs={12} sm={3} lg={2} style={{padding: '40px 20px'}}>
-          <Card icon={Checkmark} altText='checkmark icon' text='Test & Exam Services' link='http://www.ace.utoronto.ca/tes/' newPage/>
-          <Card icon={[TSJ, TS]} altText={['TSJ icon', 'teaching station icon']} text={['Teaching Station Jr.', 'Teaching Station']} link='teaching_station1.html'/>
-          <Card icon={Conhall} altText='con hall icon' text='Con Hall Website' link='http://ace.utoronto.ca/conhall/' newPage/>
-        </Col>
+    return <div style={{minHeight: '100%'}}>
+      <Grid fluid>
+        <Row>
+          <Header/>
+        </Row>
+        <Row className={classes.body}>
+          <Col xs={12} sm={3} lg={2} style={{padding: '40px 20px'}}>
+            <Card icon={Checkmark} altText='checkmark icon' text='Test & Exam Services' link='http://www.ace.utoronto.ca/tes/' newPage/>
+            <Card icon={[TSJ, TS]} altText={['TSJ icon', 'teaching station icon']} text={['Teaching Station Jr.', 'Teaching Station']} link='teaching_station1.html'/>
+            <Card icon={Conhall} altText='con hall icon' text='Con Hall Website' link='http://ace.utoronto.ca/conhall/' newPage/>
+          </Col>
 
-        <Col xs={12} sm={6} lg={8} style={{padding: '40px 40px 0 40px'}}>
-          <h1>Academic + Campus Events</h1>
-          <HLink
-            color='#002A5C'
-            fontSize='1.1em'
-            link='http://www.utoronto.ca'
-            newPage uppercase>University of Toronto, Downtown Campus
-          </HLink>
-          <Table>
-            <TableRow width='30%' data={['Room Reservation', 'To reserve classroom space or look up existing reservations']} link='room_res.html'/>
-            <TableRow data={['Room Description', 'Classroom descriptions and photographs']} link='../class_spec'/>
-            <TableRow data={['Tech Support', 'Classroom and A/V technology support']} link='tech_support.html'/>
-            <TableRow data={['Report Classroom Problem', 'Online form for classroom and A/V equipment problems']} link='http://www.ace.utoronto.ca/comments'/>
-            <TableRow data={['Weddings', 'To apply for wedding photography on campus']} link='weddinghome.html'/>
-            <TableRow data={['Filming', 'To apply for commercial filming on campus']} link='film.html'/>
-            <TableRow data={['Accessibility', 'Building and meeting room accessibility']} link='accessibility.html'/>
-          </Table>
-        </Col>
+          <Col xs={12} sm={6} lg={8} style={{padding: '40px 40px 0 40px'}}>
+            <h1>Academic + Campus Events</h1>
+            <HLink
+              color='#002A5C'
+              fontSize='1.1em'
+              link='http://www.utoronto.ca'
+              newPage uppercase>University of Toronto, Downtown Campus
+            </HLink>
+            <Table>
+              <TableRow width='30%' data={['Room Reservation', 'To reserve classroom space or look up existing reservations']} link='room_res.html'/>
+              <TableRow data={['Room Description', 'Classroom descriptions and photographs']} link='../class_spec'/>
+              <TableRow data={['Tech Support', 'Classroom and A/V technology support']} link='tech_support.html'/>
+              <TableRow data={['Report Classroom Problem', 'Online form for classroom and A/V equipment problems']} link='http://www.ace.utoronto.ca/comments'/>
+              <TableRow data={['Weddings', 'To apply for wedding photography on campus']} link='weddinghome.html'/>
+              <TableRow data={['Filming', 'To apply for commercial filming on campus']} link='film.html'/>
+              <TableRow data={['Accessibility', 'Building and meeting room accessibility']} link='accessibility.html'/>
+            </Table>
+          </Col>
 
-        <Col xs={12} sm={3} lg={2} style={{padding: '40px 20px'}}>
-          <Card text='Report Classroom Problem' link='http://www.ace.utoronto.ca/comments' newPage/>
-          <Card text='ACE Academic Calendar 2017-2018' link='http://www.ace.utoronto.ca/website/PDFs/ACE Calendar 2017-2018.pdf' newPage/>
-          <ContactCard/>
-        </Col>
-      </Row>
-      <Row className={classes.footer}>
-        <Footer/>
-      </Row>
-    </Grid>
+          <Col xs={12} sm={3} lg={2} style={{padding: '40px 20px'}}>
+            <Card text='Report Classroom Problem' link='http://www.ace.utoronto.ca/comments' newPage/>
+            <Card text='ACE Academic Calendar 2017-2018' link='http://www.ace.utoronto.ca/website/PDFs/ACE Calendar 2017-2018.pdf' newPage/>
+            <ContactCard/>
+          </Col>
+        </Row>
+        <Row className={classes.footer}>
+          <Footer/>
+        </Row>
+      </Grid>
+    </div>
   }
 }
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import classes from './Footer.css';
 import FooterNavItems from './FooterNavItems/FooterNavItems';
 
-const footer = props => (<footer className={classes.footer}>
+const footer = props => (<footer>
   <nav>
     <FooterNavItems/>
   </nav>
